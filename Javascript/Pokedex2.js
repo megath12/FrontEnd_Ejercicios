6586471159d6c72e2210db3abb5fcc931a634815
Carlos Iturbe Gil <39778547+megath12@users.noreply.github.com>
2022-04-07 23:51:51 -0500

    Add files via upload

---
+++ Javascript/Pokedex2.js
@@ -0,0 +1,31 @@
+
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("logopoke.png")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+
+
